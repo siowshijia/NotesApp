c2351749384d1e6101d7f04864152dd2331951b3
shijia <siowshijia@gmail.com>
2024-01-27 03:24:03 +0800

    comment truncated text to 20 character and arrow

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -5,13 +5,13 @@ import {homeStyles} from '../styles/home';
 import {space} from '../styles/space';
 
 const NoteItem = ({note}) => {
-  const truncatedContent =
-    note.content.length > 20 ? note.content.slice(0, 20) + '...' : note.content;
+  // const truncatedContent =
+  //   note.content.length > 20 ? note.content.slice(0, 20) + '...' : note.content;
 
   return (
     <View key={note.id} style={homeStyles.noteItem}>
-      <Text style={homeStyles.noteItemText}>{truncatedContent}</Text>
-      <IconArrow width={20} height={20} style={{marginLeft: space.m4}} />
+      <Text style={homeStyles.noteItemText}>{note.content}</Text>
+      {/* <IconArrow width={20} height={20} style={{marginLeft: space.m4}} /> */}
     </View>
   );
 };
